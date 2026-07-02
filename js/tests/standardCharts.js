844f2040779d10cdedc1711fc983bd81e8429478
SY <lsy9249638@gmail.com>
2026-07-02 15:30:41 +0800

    v2.1.0 - Add Standard Chart Test Suite

---
+++ js/tests/standardCharts.js
@@ -0,0 +1,35 @@
+const STANDARD_CHARTS = [
+  {
+    id: "TEST-001",
+    source: "羅一鳴老師命盤",
+    solarDate: "2026-07-02",
+    solarTime: "14:06",
+
+    expected: {
+      yearGanZhi: "",
+      monthGanZhi: "",
+      dayGanZhi: "",
+      hourGanZhi: "",
+
+      solarTerm: "",
+      juDisplay: "",
+      xunShou: "",
+      xunShouStem: "",
+
+      zhiFu: "",
+      zhiShi: "",
+
+      palaces: {
+        1: {},
+        2: {},
+        3: {},
+        4: {},
+        5: {},
+        6: {},
+        7: {},
+        8: {},
+        9: {}
+      }
+    }
+  }
+];
